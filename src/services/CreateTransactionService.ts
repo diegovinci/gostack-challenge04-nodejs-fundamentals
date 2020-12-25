38f82aff65f0e541fbf52fd0448f82ce264fe55a
diegovinci <diego.vinci@outlook.com>
2020-12-25 17:26:39 -0300

    complete the task

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,38 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  private checkSufficientFunds(outcome: number): boolean {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (outcome > total) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Invalid transaction type');
+    }
+
+    if (typeof value !== 'number') {
+      throw Error('Transaction value should be a number');
+    }
+
+    if (type === 'outcome') {
+      if (!this.checkSufficientFunds(value)) {
+        throw Error('Insufficient funds');
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
